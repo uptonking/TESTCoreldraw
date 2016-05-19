ab03f166cbc062d21a01c4cf0fa64b82735765d3
uptonking <jinyaoo86@gmail.com>
2016-05-19 23:38:13 +0800

    CDR转PDF，控制台C#程序

---
+++ CdrToPdf/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Corel.Interop.CorelDRAW;
+
+namespace CdrToPdf
+{
+	class Program
+	{
+		public static void Main(String[] args)
+		{
+            String source = @"C:\Users\Yao\Documents\Corel\iphone.cdr";
+			
+			String release = @"C:\Users\Yao\Documents\Corel\new.pdf";
+			
+			Application application = new Application();
+			
+			Document document = (Document)application.OpenDocument(source, 1);
+			
+			document.PublishToPDF(release);
+			
+			document.Close();
+		}
+	}
+}
